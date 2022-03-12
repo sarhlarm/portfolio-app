6ef70cb58e09356d9140028ad6653b2c93fa5e1e
sarhlarm <98288904+sarhlarm@users.noreply.github.com>
2022-03-12 18:03:53 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install gcc g++ make
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs -y
-sudo npm install -g forever
+sudo npm install -g pm2
 # sudo npm i dotenv
 # sudo npm audit fix --force
 
