5382f5975ebe0b8c9ec87b95987a69a6e19a5122
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-12 11:42:38 -0600

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,6 @@ sudo apt-get update
 sudo apt-get install gcc g++ make
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs -y
-sudo npm install
 sudo npm install -g forever
 # sudo npm i dotenv
 # sudo npm audit fix --force
